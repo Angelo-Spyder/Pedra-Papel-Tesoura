6a87677b775dc4349c866df8466f66117ff07257
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-04-01 20:54:28 -0300

    Adicionando eventos de clique e jogada aleatoria de evento

---
+++ main.js
@@ -0,0 +1,28 @@
+
+var pedra = document.querySelector("#pedra");
+var papel = document.querySelector("#papel");
+var tesoura = document.querySelector("#tesoura");
+
+var jogadaIA = document.querySelector(".jogada-ia");
+
+pedra.addEventListener("click", function(){  
+    jogadaAleatoria()
+})
+
+papel.addEventListener("click", function(){  
+    jogadaAleatoria()
+})
+
+tesoura.addEventListener("click", function(){  
+    jogadaAleatoria()
+})
+
+function jogadaAleatoria(){
+    var opcoes = [`<img class="pedra" src="images/pedra.png" alt="Mão-sinal-de-pedra">`,
+    `<img class="pedra" src="images/papel.png" alt="Mão-sinal-de-papel">`,
+    `<img class="pedra" src="images/tesoura.png" alt="Mão-sinal-de-tesoura">`]
+
+    var aleatoria = Math.floor(Math.random() * 3) 
+    
+    jogadaIA.innerHTML = opcoes[aleatoria];
+}
