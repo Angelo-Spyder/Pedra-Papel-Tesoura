9c22c8b32a1087f09f5a1d0c7f2b445366242c66
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-04-02 21:01:47 -0300

    Adicionando aplicação de placar na opção pedra

--- main.js
+++ main.js
@@ -6,11 +6,20 @@ var tesoura = document.querySelector("#tesoura");
 var JogadaP = document.querySelector(".jogada-player")
 var jogadaIA = document.querySelector(".jogada-ia");
 
+var placarP = document.querySelector(".placar-player")
+var placarIA = document.querySelector(".placar-ia")
+
 pedra.addEventListener("click", function(){ 
     JogadaP.innerHTML = "" 
     JogadaP.innerHTML = `<img class="pedra" src="images/pedra.png" alt="Mão-sinal-de-pedra">`
     JogadaP.classList.add("opcao-de-jogada")
+
     jogadaAleatoria()
+    if(jogadaIA.innerHTML == `<img class="pedra" src="images/tesoura.png" alt="Mão-sinal-de-tesoura">`){
+        placarP.textContent = Number(placarP.textContent) + 1
+    }else if(jogadaIA.innerHTML == `<img class="pedra" src="images/papel.png" alt="Mão-sinal-de-papel">`){
+        placarIA.textContent = Number(placarIA.textContent) + 1
+    }
 })
 
 papel.addEventListener("click", function(){  
